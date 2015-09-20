5153db99e6d527ae7feca14f2a7bf7dbe19a19d4
mtjohns1 <mtjohns1@mtu.edu>
2015-09-20 15:07:21 -0400

    setup

---
+++ Assets/Scripts/Finder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finder : MonoBehaviour {
+    public Transform target;
+    Vector3 destination;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        // Cache agent component and destination
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
+    }
+
+    void Update()
+    {
+        // Update destination if the target moves one unit
+        
+        if (Vector3.Distance(destination, target.position) > 1.0f)
+        {
+           
+            destination = target.position;
+           
+            agent.destination = destination;
+        }
+    }
+    /*
+	public Transform destinationPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.GetComponent<NavMeshAgent> ().destination = destinationPoint.position;
+	}*/
+}
